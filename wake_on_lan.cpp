fa38d120685ce77860aaa9781092effc0e6ee1c5
twergi <110980688+twergi@users.noreply.github.com>
2023-07-15 12:45:04 +0300

    delete unused libraries

--- wake_on_lan.cpp
+++ wake_on_lan.cpp
@@ -1,14 +1,7 @@
 // C program to remotely Power On a PC over the
 // internet using the Wake-on-LAN protocol.
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
-#include <sys/types.h>
-
 #include <iostream>
 #include <stdexcept>
 
